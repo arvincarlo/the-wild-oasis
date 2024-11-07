48a5555a56846281769ad3080c0409f4f0f3450b
Arvin <arvincris@gmail.com>
2024-11-07 11:42:33 +0800

    Introduction to Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: red;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #BAD455;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: salmon;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <StyledApp>
+      <H1>The Wild Oasis!</H1>
+      <Button onClick={() => alert("check in")}>Check in</Button>
+      <Button onClick={() => alert("check out")}>Check out</Button>
+
+      <Input type="number" placeholder="Number of guests"></Input>
+      <Input type="number" placeholder="Number of guests"></Input>
+      <Input type="number" placeholder="Number of guests"></Input>
+    </StyledApp>
   )
 }
 
