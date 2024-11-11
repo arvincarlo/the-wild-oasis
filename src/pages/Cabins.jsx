2db9918f1e49593f3af0fecffe8957bbe45cf1b3
Arvin <arvincris@gmail.com>
2024-11-11 12:03:17 +0800

    Fetching Cabin Data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
 
-  // Get the cabin data
-  useEffect(() => {
-    getCabins().then(response => console.table(response))
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://faszoicjvndehdqpwugm.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" alt="" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
