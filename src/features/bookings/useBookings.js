5ea52e0cb990f8350982f6cfc6ba50451ea670e7
Arvin Cris <arvincris97@gmail.com>
2024-12-29 22:14:09 +0800

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings"
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+    const [searchParams] = useSearchParams();
+
+    // FILTER
+    const filteredValue = searchParams.get("status");
+    const filterDetails = [
+        { method: "eq", field: "status", value: filteredValue }
+    ];
+    const filter = !filteredValue || filteredValue === 'all' ? null : filterDetails;
+
+    // SORT
+    const sortByRaw = searchParams.get('sortBy') || "startDate-desc";
+    const [ field, direction ] = sortByRaw.split('-');
+    const sortBy = { field, direction };
+
     const { data: bookings, isLoading, error } = useQuery({
-        queryKey: ["bookings"],
-        queryFn: getBookings
+        queryKey: ["bookings", filter, sortBy],
+        queryFn: () => getBookings({filter, sortBy})
     });
 
     return { bookings, isLoading, error}
